4e28ae5c17a03be5262bf1eaef1746087f9c495d
Stacey Ali <96086935+Slam256@users.noreply.github.com>
2022-08-26 23:37:14 -0400

    setup auth with fb

--- src/components/Subtotal.jsx
+++ src/components/Subtotal.jsx
@@ -1,10 +1,13 @@
 import '../css/Subtotal.css';
+import { useNavigate } from 'react-router-dom';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from '../StateProvider';
 import { getBasketTotal } from '../reducer';
 
 function Subtotal() {
+  const navigate = useNavigate();
   const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -25,7 +28,7 @@ function Subtotal() {
         prefix={'$'}
       />
 
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => navigate('/payment')}>Proceed to Checkout</button>
     </div>
   );
 }
